dcfe18e04fb592d96174664a6fc149d4d84169c0
Tr4shL0rd <ketil04@gmail.com>
2022-01-26 16:07:35 +0100

    Added better comments

--- apodDownloader.py
+++ apodDownloader.py
@@ -11,6 +11,8 @@ import json
 load_dotenv()
 apiKey = os.getenv("API_KEY")
 
+
+# checks if apikey is valid
 if helper.apikeyFound(apiKey) == False:
     if json.load(open("data.json"))["API_KEY"] == None:
         print("API key not found")
@@ -27,17 +29,20 @@ if helper.apikeyFound(apiKey) == False:
 helper.verifyApiKey(apiKey)
 url = f"https://api.nasa.gov/planetary/apod?api_key={apiKey}" 
 
-
+#fetches the data from the api and parses it into a dictionary
 r = requests.get(url).json()
+#gets hd image
 image = r["hdurl"]
-today = datetime.datetime.now().strftime('%Y-%m-%d')
 
+today = datetime.datetime.now().strftime('%Y-%m-%d')
+#gets current directory and creates a file in that directory with a name of today's date
 home = os.path.expanduser("~")
 dir = os.path.dirname(os.path.abspath(__file__))
 path = os.path.join(home,dir)
 imageName = f"apod_{today}.jpg"
 filename = os.path.join(path,imageName)
 
+#downloads the image
 urllib.request.urlretrieve(image, filename)
 print(f"saved to {filename}")
 
